841533a142348e9624e6acff67e3d627d66bd974
aqestd <amanbeko.a2005@gmail.com>
2022-02-07 00:59:55 +0600

    1

---
+++ app/src/main/java/com/example/myapplication/Fragment1.java
@@ -0,0 +1,39 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class Fragment1 extends Fragment implements View.OnClickListener {
+        Button gotoFragment2;
+
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_1, container, false);
+
+            gotoFragment2 = view.findViewById(R.id.fragment_fragment1_gotofragment2);
+            gotoFragment2.setOnClickListener(this);
+
+            return view;
+        }
+
+        @Override
+        public void onClick(View view) {
+            Navigation.findNavController(view).navigate(R.id.action_fragment1_to_fragment2);
+        }
+}
